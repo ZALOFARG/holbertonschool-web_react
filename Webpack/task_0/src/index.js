6074cce0daa81ce6ddf0060b4853892858c969c6
ZALOFARG <andre.work.go@gmail.com>
2024-06-15 11:08:33 -0500

    feat: create and run webpack using a basic installation

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data fo the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
