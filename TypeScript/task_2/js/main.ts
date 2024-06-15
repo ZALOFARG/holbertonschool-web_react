8cf65b0f075732300672757624f280af379fcd96
ZALOFARG <andre.work.go@gmail.com>
2024-06-15 08:52:04 -0500

    feat: create a string literal to use it in a funct

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -46,3 +46,28 @@ function createEmployee(salary: number | string): Director | Teacher {
     return new Director();
     }
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+    }
+}
+
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  switch (todayClass) {
+    case 'Math':
+      return 'Teaching Math';
+    case 'History':
+      return 'Teaching History';
+    default:
+      return 'Invalid subject';
+  }
+}
