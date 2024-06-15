c61526511298b243b7548753c1b325b17abe048e
ZALOFARG <andre.work.go@gmail.com>
2024-06-15 18:44:54 -0500

    fix: add some other specifications

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpg|jpeg|gif)$/i,
         use: [
           'file-loader',
           {
